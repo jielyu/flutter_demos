c6bc50ece77c30485d32a740fc5dff7a6eb04d74
jielyu <jiejielyu@outlook.com>
2022-03-14 17:29:45 +0800

    feat(flutter_package_s01): 增加flutter_statusbarcolor_ns和webview_flutter

---
+++ flutter_package_s01/android/app/src/main/kotlin/com/example/flutter_package_s01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_package_s01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_package_s01/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+
+import 'webview_flutter_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final _routes = <String, WidgetBuilder>{
+    "/home": (context) => const HomePage(),
+    "/webview_flutter": (context) => const WebviewPage(),
+  };
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      routes: _routes,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isStatusWhite = true;
+  Widget _buildListItem(String name, IconData icon) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.grey,
+          child: ListTile(
+            leading: Icon(icon),
+            onTap: () {
+              print(name);
+              Navigator.of(context).pushNamed("/" + name);
+            },
+            title: Text(name),
+          ),
+        ),
+        const Divider(
+          height: 1,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("有用的Flutter"),
+      ),
+      body: ListView(
+        children: [
+          _buildListItem("webview_flutter", Icons.web),
+          _buildListItem("provider", Icons.person),
+          _buildListItem("image_picker", Icons.person),
+          ListTile(
+            leading: const Icon(Icons.color_lens),
+            title: const Text("flutter_statusbarcolor_ns"),
+            onTap: () {
+              // 设置状态栏的颜色
+              _isStatusWhite = !_isStatusWhite;
+              FlutterStatusbarcolor.setStatusBarWhiteForeground(_isStatusWhite);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
