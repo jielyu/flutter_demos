5785940cb2d4604da1f0f31f41a1e942042294a3
jielyu <jiejielyu@outlook.com>
2022-03-17 17:14:18 +0800

    feat(flutter_package_s01): 增加colorpicker

--- flutter_package_s01/lib/color_picker_page.dart
+++ flutter_package_s01/lib/color_picker_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class ColorPickerPage extends StatefulWidget {
   const ColorPickerPage({Key? key}) : super(key: key);
@@ -7,7 +8,78 @@ class ColorPickerPage extends StatefulWidget {
   State<ColorPickerPage> createState() => _ColorPickerPageState();
 }
 
+enum PickerType {
+  colorPickerType,
+  materialPickerType,
+  blockPickerType,
+  slidePickerType,
+  // multipleChoiceBlockPickerType,
+}
+
 class _ColorPickerPageState extends State<ColorPickerPage> {
+  // create some values
+  Color pickerColor = const Color(0xff443a49);
+  Color currentColor = const Color(0xff443a49);
+
+  // ValueChanged<Color> callback
+  void changeColor(Color color) {
+    setState(() => pickerColor = color);
+  }
+
+  void onShowDialog(PickerType pickerType) {
+    Widget picker = Container();
+    switch (pickerType) {
+      case PickerType.colorPickerType:
+        picker = ColorPicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+        );
+        break;
+      case PickerType.materialPickerType:
+        picker = MaterialPicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+        );
+        break;
+      case PickerType.blockPickerType:
+        picker = BlockPicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+        );
+        break;
+      // case PickerType.multipleChoiceBlockPickerType:
+      // picker = MultipleChoiceBlockPicker(
+      //     pickerColor: pickerColor,
+      //     onColorChanged: changeColor,
+      //   );
+      //   break;
+      case PickerType.slidePickerType:
+        picker = SlidePicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+        );
+        break;
+    }
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('选择颜色'),
+        content: SingleChildScrollView(
+          child: picker,
+        ),
+        actions: <Widget>[
+          ElevatedButton(
+            child: const Text('确定'),
+            onPressed: () {
+              setState(() => currentColor = pickerColor);
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +88,47 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: const Text("颜色拾取器"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                onShowDialog(PickerType.colorPickerType);
+              },
+              child: Text(
+                "调色板",
+                style: TextStyle(color: currentColor),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                onShowDialog(PickerType.blockPickerType);
+              },
+              child: Text(
+                "颜色标签",
+                style: TextStyle(color: currentColor),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                onShowDialog(PickerType.materialPickerType);
+              },
+              child: Text(
+                "物料颜色",
+                style: TextStyle(color: currentColor),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                onShowDialog(PickerType.slidePickerType);
+              },
+              child: Text(
+                "滑块调色",
+                style: TextStyle(color: currentColor),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
