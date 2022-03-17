070e0f04b0330d13dd6c93345e1ea6cd74b8f9bc
jielyu <jiejielyu@outlook.com>
2022-03-17 16:24:42 +0800

    feat(flutter_package_s01): 增加video_player

--- flutter_package_s01/lib/video_player_page.dart
+++ flutter_package_s01/lib/video_player_page.dart
@@ -9,6 +9,36 @@ class VideoPlayerPage extends StatefulWidget {
 }
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..addListener(() async {
+        if (_controller.value.hasError) {
+          print("播放出错");
+        } else {
+          Duration? res = await _controller.position;
+          if (res! >= _controller.value.duration) {
+            await _controller.seekTo(const Duration(seconds: 0));
+            await _controller.pause();
+            setState(() {});
+          }
+        }
+      })
+      ..initialize().then((value) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +47,26 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: const Text("视频播放器"),
+        child: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
       ),
     );
   }
